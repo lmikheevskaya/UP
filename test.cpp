6406b2c6a609572c030a171fe701bfff19e27866
lmikheevskaya <lmikheevskaya@gmail.com>
2016-08-03 11:15:41 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,12 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	return 0;
+}
+
